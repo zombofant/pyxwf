511d9eccc737dde6c8b3960b2ef75b537f4574b8
Jonas Wielicki <j.wielicki@sotecware.net>
2012-08-02 11:34:52 +0200

    Add force-show-current attribute to Breadcrumbs
    Also, breadcrumbs have been reworked to use modern approaches

--- PyWeb/Crumbs/Breadcrumbs.py
+++ PyWeb/Crumbs/Breadcrumbs.py
@@ -3,7 +3,8 @@ import PyWeb.utils as utils
 import PyWeb.Namespaces as NS
 import PyWeb.Registry as Registry
 import PyWeb.Crumbs as Crumbs
-import PyWeb.Navigation as Nav
+import PyWeb.Navigation as Navigation
+import PyWeb.Types as Types
 
 class Breadcrumbs(Crumbs.CrumbBase):
     __metaclass__ = Registry.CrumbMeta
@@ -17,23 +18,30 @@ class Breadcrumbs(Crumbs.CrumbBase):
             self.root = site.getNode(rootID)
         else:
             self.root = None
+        self.forceShowCurrent = Types.Typecasts.bool(
+            node.get("force-show-current", False))
 
     def render(self, ctx):
         ul = ET.Element(NS.XHTML.ul)
-        node = ctx.pageNode
         hadNodes = set()
-        while node is not self.root and node is not None:
+        for node in ctx.pageNode.iterUpwards():
+            if node is self.root:
+                break
             navInfo = node.getNavigationInfo(ctx)
-            if navInfo.getDisplay() == Nav.Show:
-                representative = navInfo.getRepresentative()
-                if not representative in hadNodes:
-                    hadNodes.add(representative)
-                    li = ET.Element(NS.XHTML.li)
-                    if node is not ctx.pageNode:
-                        a = ET.SubElement(li, NS.PyWebXML.a, href=representative.Path)
-                        a.text = navInfo.getTitle()
-                    else:
-                        li.text = navInfo.getTitle()
-                    ul.insert(0, li)
-            node = node.parent
+            if navInfo.getDisplay() != Navigation.Show and (
+                    node is not ctx.pageNode or not self.forceShowCurrent):
+                continue
+
+            representative = navInfo.getRepresentative()
+            if representative in hadNodes:
+                continue
+            
+            hadNodes.add(representative)
+            li = ET.Element(NS.XHTML.li)
+            if node is not ctx.pageNode:
+                a = ET.SubElement(li, NS.PyWebXML.a, href=representative.Path)
+                a.text = navInfo.getTitle()
+            else:
+                li.text = navInfo.getTitle()
+            ul.insert(0, li)
         return ul
