231d2a084c4f00205d01c0577536cb3a8c07413a
Jonas Wielicki <j.wielicki@sotecware.net>
2012-08-25 16:03:38 +0200

    Add some test for charset detection

--- tests/test_Context.py
+++ tests/test_Context.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 from __future__ import unicode_literals
 
 import unittest
@@ -7,15 +8,15 @@ import PyXWF.Message as Message
 import tests.Mocks as Mocks
 
 class Context(unittest.TestCase):
-    def sendMessage(self, **kwargs):
+    def sendMessage(self, body="Foo bar", **kwargs):
         ctx = Mocks.MockedContext("/", **kwargs)
-        message = Message.TextMessage("Foo bar", encoding="utf-8")
+        message = Message.TextMessage(body, encoding="utf-8")
         ctx.sendResponse(message)
         return ctx
 
     def test_message(self):
         ctx = self.sendMessage()
-        self.assertEqual(ctx.Out.getvalue(), """\
+        self.assertEqual(ctx.Out.getvalue(), b"""\
 200 Mocked Status Code
 vary: host
 content-type: text/plain; charset=utf-8
@@ -24,9 +25,28 @@ Foo bar""")
 
     def test_charsetDetection(self):
         ctx = self.sendMessage(acceptCharset="ascii,utf-8;q=0.9")
-        self.assertEqual(ctx.Out.getvalue(), """\
+        self.assertEqual(ctx.Out.getvalue(), b"""\
 200 Mocked Status Code
 vary: host
 content-type: text/plain; charset=ascii
 
 Foo bar""")
+
+    def test_charsetFallback(self):
+        ctx = self.sendMessage(body="äöü", acceptCharset="ascii")
+        self.assertEqual(ctx.Out.getvalue(), b"""\
+200 Mocked Status Code
+vary: host
+content-type: text/plain; charset=utf-8
+
+"""+"""äöü""".encode("utf-8"))
+
+    def test_charsetDetection_complex(self):
+        ctx = self.sendMessage(body="😸", acceptCharset="ascii,latin-1;q=0.9,utf-32be;q=0.8,utf-32le;q=0.8,utf-8;q=0.7")
+        self.assertEqual(ctx.Out.getvalue(), b"""\
+200 Mocked Status Code
+vary: host
+content-type: text/plain; charset=utf-32le
+
+"""+"""😸""".encode("utf-32le"))
+
