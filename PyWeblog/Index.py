zombofant/pyxwf

commit 16acfe43df7bb3f2f49a15e5d11f65aa25a99498

Rework blog. Sadly, this depends on future changes
Sorry, I messed up my stash

PyWeblog/Index.py
@@ -1,0 +1,373 @@
+from __future__ import unicode_literals, print_function, absolute_import
+
+import functools, os, logging, operator
+
+from PyXWF.utils import ET, BraceMessage as _F, blist
+import PyXWF.Resource as Resource
+import PyXWF.Errors as Errors
+import PyXWF.Namespaces as NS
+
+SortedPostList = blist.sortedlist
+
+@functools.total_ordering
+class Post(Resource.Resource):
+    def __init__(self, cache, fileName, pathFormat, dateFormat,
+            resortCallback=None, findNeighboursCallback=None):
+        super(Post, self).__init__()
+        self.cache = cache
+        self.fileName = fileName
+
+        self.basename = os.path.basename(self.fileName)
+        self.title = None
+        self.authors = None
+        self.keywords = None
+        self.creationDate = None
+        self.description = None
+        self._prevPost = None
+        self._nextPost = None
+
+        self._resortCallback = resortCallback
+        self._pathFormat = pathFormat
+        self._dateFormat = dateFormat
+        self._cacheMetadata(self.cache[self.fileName].doc)
+        self._lastModified = self._calcLastModified()
+        self._findNeighboursCallback = findNeighboursCallback
+
+    def _cacheMetadata(self, document):
+        creationDate = document.date
+        authors = document.authors
+        keywords = document.keywords
+        description = document.description or ""
+        title = document.title
+
+        self.needResort = False
+        if self.creationDate is not None:
+            if      creationDate.year != self.creationDate.year or \
+                    creationDate.month != self.creationDate.month:
+                self.needResort = True
+        if self.keywords is not None:
+            if frozenset(keywords) != frozenset(self.keywords):
+                self.needResort = True
+
+        """if self.authors is not None:
+            if authors != self.authors:
+                self.needResort = True"""
+
+        if self.needResort and self._resortCallback:
+            self._resortCallback(self, creationDate, authors, keywords)
+
+        self.creationDate = creationDate
+        self.authors = authors
+        self.keywords = keywords
+        self.description = description
+        self.title = title
+        self.path = self._pathFormat.format(
+            year=self.creationDate.year,
+            month=self.creationDate.month,
+            day=self.creationDate.day,
+            basename=self.basename
+        )
+
+        self.abstract = NS.PyWebXML("meta",
+            NS.PyWebXML("title", self.title),
+            NS.PyWebXML("description", self.description),
+            NS.PyBlog("node-path", self.path),
+        )
+        ET.SubElement(self.abstract, NS.PyWebXML.date, attrib={
+            getattr(NS.PyBlog, "formatted"): self.creationDate.strftime(self._dateFormat)
+        }).text = self.creationDate.isoformat()+"Z"
+        for keyword in self.keywords:
+            ET.SubElement(self.abstract, NS.PyWebXML.kw).text = keyword
+        for author in self.authors:
+            author.applyToNode(ET.SubElement(self.abstract, NS.PyWebXML.author))
+
+        # mark the relations for update
+        if self._prevPost:
+            self._prevPost._nextPost = False
+        if self._nextPost:
+            self._nextPost._prevPost = False
+        self._prevPost = False
+        self._nextPost = False
+
+    def _calcLastModified(self):
+        return self.cache.getLastModified(self.fileName)
+
+    def _updateNeighbours(self):
+        if self._findNeighboursCallback:
+            self._prevPost, self._nextPost = self._findNeighboursCallback(self)
+        else:
+            self._prevPost, self._nextPost = None, None
+
+    @property
+    def LastModified(self):
+        return self._lastModified
+
+    @property
+    def PrevPost(self):
+        if self._prevPost is False:
+            self._updateNeighbours()
+        return self._prevPost
+
+    @property
+    def NextPost(self):
+        if self._nextPost is False:
+            self._updateNeighbours()
+        return self._nextPost
+
+    def update(self):
+        newLastModified = self._calcLastModified()
+        if newLastModified > self._lastModified:
+            doc = self.cache[self.fileName].doc
+            self._cacheMetadata(doc)
+            self._lastModified = newLastModified
+
+    def getDocument(self):
+        return self.cache.get(self.fileName, headerOffset=2).doc
+
+    def getPyWebXML(self):
+        page = self.getDocument().toPyWebXMLPage()
+        meta = page.find(NS.PyWebXML.meta)
+        date = meta.find(NS.PyWebXML.date)
+        if date is not None:
+            date.set(NS.PyBlog.formatted, self.creationDate.strftime(self._dateFormat))
+
+        prevPost = self.PrevPost
+        nextPost = self.NextPost
+        if prevPost:
+            ET.SubElement(meta, getattr(NS.PyBlog, "prev-post"), attrib={
+                "href": prevPost.path,
+                "title": prevPost.title
+            })
+        if nextPost:
+            ET.SubElement(meta, getattr(NS.PyBlog, "next-post"), attrib={
+                "href": nextPost.path,
+                "title": nextPost.title
+            })
+        return page
+
+    def __lt__(self, other):
+        try:
+            return self.creationDate < other.creationDate
+        except AttributeError:
+            return NotImplemented
+
+    def __eq__(self, other):
+        try:
+            # this should actually _always_ be the case if the filename matches
+            # but to be safe we first compare for the creationDate equality...
+            return  self.creationDate == other.creationDate and \
+                    self.fileName == other.fileName
+        except AttributeError:
+            return NotImplemented
+
+
+class Index(Resource.Resource):
+    def __init__(self, blog, docCache, entryDir, pathFormat, dateFormat,
+            postsChangedCallback=None):
+        super(Index, self).__init__()
+        self._docCache = docCache
+        self._dir = entryDir
+        self._posts = SortedPostList()
+        self._calendary = {}
+        self._keywords = {}
+        self._postFiles = {}
+        self._lastModified = None
+        self._pathFormat = pathFormat
+        self._dateFormat = dateFormat
+        self._postsChangedCallback = postsChangedCallback
+
+    def _reload(self):
+        logging.info("Updating blog index")
+
+        ignoreNames = frozenset(["blog.reload", "blog.index"])
+
+        missing = set(self._postFiles.iterkeys())
+
+        added, updated, errors = 0, 0, 0
+
+        for dirpath, dirnames, filenames in os.walk(self._dir):
+            for filename in filenames:
+                if filename in ignoreNames:
+                    continue
+
+                fullPath = os.path.join(dirpath, filename)
+                # first, check if we already know the file. If that's the case,
+                # we only do an update.
+                try:
+                    post = self._postFiles[fullPath]
+                except KeyError:
+                    pass
+                else:
+                    missing.remove(fullPath)
+                    updated += 1
+                    continue
+                # otherwise, we'll load and add the post if possible.
+                try:
+                    self.addPost(fullPath)
+                    added += 1
+                except (Errors.MissingParserPlugin,
+                        Errors.UnknownMIMEType) as err:
+
+                    logging.warning(_F("While loading blog post at {1!r}: {0}",\
+                                       err, filename))
+                    errors += 1
+                except ValueError as err:
+                    logging.error(_F("While loading blog post at {1!r}: {0}", \
+                                     err, filename))
+                    errors += 1
+
+        for fileName in missing:
+            post = self._postFiles[fileName]
+            self.remove(post)
+        try:
+            self._lastModified = max(map(operator.attrgetter("LastModified"),
+                                         self._posts))
+        except ValueError:
+            self._lastModified = None
+            logging.warning(_F("No blog posts found in {0}", self._dir))
+
+        if len(missing) or added or updated or errors:
+            if self._postsChangedCallback:
+                self._postsChangedCallback()
+            logging.info(_F(
+    "Updated blog index; {0} removed, {1} added, {2} updated, {3} errors",
+                len(missing),
+                added,
+                updated,
+                errors
+            ))
+
+    @property
+    def LastModified(self):
+        if self._lastModified is None:
+            self._reload()
+        return self._lastModified
+
+    def update(self):
+        # todo
+        pass
+
+    def _autocreateMonthDir(self, year, month):
+        try:
+            yearDir = self._calendary[year]
+        except KeyError:
+            yearDir = [SortedPostList() for i in range(12)]
+            self._calendary[year] = yearDir
+        monthDir = yearDir[month-1]
+        return monthDir
+
+    def _autocreateKeywordDir(self, keyword):
+        try:
+            return self._keywords[keyword]
+        except KeyError:
+            keywordDir = SortedPostList()
+            self._keywords[keyword] = keywordDir
+            return keywordDir
+
+    def _findNeighbours(self, post):
+        idx = self._posts.index(post)
+        if idx > 0:
+            prev = self._posts[idx-1]
+        else:
+            prev = None
+        if idx < len(self._posts)-1:
+            next = self._posts[idx+1]
+        else:
+            next = None
+        return prev, next
+
+    def _unindexPost(self, post):
+        year, month = post.creationDate.year, post.creationDate.month
+        self._calendary[year][month-1].remove(post)
+        for keyword in post.keywords:
+            self._keywords[keyword].remove(post)
+
+    def _removePost(self, post):
+        self._unindexPost(post)
+        self._posts.remove(post)
+
+    def _resortPost(self, post, newDate, newAuthors, newKeywords):
+        self._unindexPost(post)
+
+        year, month = newCreationDate.year, newCreationDate.month
+        self._autocreateMonthDir(year, month).add(post)
+        for keyword in newKeywords:
+            self._autocreateKeywordDir(keyword).add(post)
+
+    def addPost(self, fileName):
+        post = Post(self._docCache, fileName, self._pathFormat,
+                self._dateFormat,
+                resortCallback=self._resortPost,
+                findNeighboursCallback=self._findNeighbours)
+        self._autocreateMonthDir(   post.creationDate.year,
+                                    post.creationDate.month).add(post)
+        for keyword in post.keywords:
+            self._autocreateKeywordDir(keyword).add(post)
+        self._posts.add(post)
+        return post
+
+    def getAllPosts(self):
+        return self._posts
+
+    def getPostsByKeyword(self, tag):
+        try:
+            return self._keywords[tag]
+        except KeyError:
+            return []
+
+    def getKeywords(self):
+        return (keyword for keyword, posts in self._keywords.viewitems() if len(posts) > 0)
+
+    def getKeywordPosts(self):
+        return filter(operator.itemgetter(1), self._keywords.viewitems())
+
+    def getPosts(self, tag=None, reverse=False):
+        if reverse:
+            return reversed(self.getPosts(tag=tag, reverse=False))
+        if tag:
+            return self.getPostsByKeyword(tag)
+        else:
+            return self.getAllPosts()
+
+    def getPostsByMonth(self, year, month):
+        try:
+            return self._calendary[year][month-1]
+        except KeyError:
+            return []
+
+    def getPostsByYear(self, year):
+        try:
+            return itertools.chain(*self._calendary[year])
+        except KeyError:
+            return []
+
+    def getPostsByYearNewestFirst(self, year):
+        try:
+            return itertools.chain(*(
+                reversed(monthDir) for monthDir in reversed(self._calendary[year])
+            ))
+        except KeyError:
+            return []
+
+    def getPostsByDate(self, year, month=None, reverse=False):
+        if month:
+            if reverse:
+                return reversed(self.getPostsByMonth)
+            else:
+                return self.getPostsByMonth()
+        else:
+            if reverse:
+                return self.getPostsByYearNewestFirst(year)
+            else:
+                return self.getPostsByYear(year)
+
+    def iterDeep(self):
+        return (
+            (
+                year,
+                (month+1
+                    for month, monthDir in enumerate(months)
+                        if len(monthDir) > 0
+                )
+            ) for year, months in self._calendary.viewitems()
+        )

PyWeblog/Atom.py
@@ -1,103 +1,35 @@
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import, print_function
 
-import operator
-
-from PyXWF.utils import ET
+import PyXWF.Registry as Registry
+import PyXWF.Namespaces as NS
 import PyXWF.ContentTypes as ContentTypes
 import PyXWF.Types as Types
-import PyXWF.Nodes as Nodes
-import PyXWF.Navigation as Navigation
-import PyXWF.Namespaces as NS
 
-class AtomFeedRoot(object):
-    def __init__(self, cfgNode, blog):
-        super(AtomFeedRoot, self).__init__()
-        self.blog = blog
-        self.Site = blog.Site
+import PyWeblog.GenericFeed as GenericFeed
 
-        self.templateName = Types.NotNone(cfgNode.get("template"))
-        self.rootId = cfgNode.get("root-id")
-        self.mapUpdatedToLastModified = Types.DefaultForNone(False,
-            Types.Typecasts.bool)(cfgNode.get("map-updated-to-last-modified"))
-        self.linkPrefix = Types.NotNone(cfgNode.get("link-prefix"))
-        self.idPrefix = cfgNode.get("id-prefix", self.linkPrefix)
-        self.limit = Types.NumericRange(Types.Typecasts.int, 1, None)(cfgNode.get("limit"))
+class Atom(GenericFeed.GenericFeed):
+    __metaclass__ = Registry.NodeMeta
 
-    @staticmethod
-    def Link(href, title=""):
-        return ET.Element(NS.XHTML.link, attrib={
-            "rel": "alternate",
-            "type": ContentTypes.Atom,
-            "href": href,
-            "title": title
-        })
+    namespace = str(NS.PyBlog)
+    names = ["atom-feed"]
 
-    def prepare(self, ctx, posts):
-        ctx.useResources(posts)
-        self.template = self.Site.templateCache[self.templateName]
-        ctx.useResource(self.template)
+    ContentType = ContentTypes.Atom
 
-    def render(self, ctx, posts, title, kind, path):
-        root = ET.Element(NS.PyBlog.syndication)
-        root.set("title", title)
-        root.set("kind", kind)
-        rootID = ET.SubElement(root, NS.PyBlog.id)
-        rootID.text = self.rootId or (self.idPrefix + self.blog.Path)
-        if self.mapUpdatedToLastModified:
-            updatedKey = operator.attrgetter("LastModified")
-        else:
-            updatedKey = operator.attrgetter("creationDate")
+    def __init__(self, site, parent, node):
+        super(Atom, self).__init__(site, parent, node)
+        self._queryValue = Types.NotEmpty(node.get("query-value", "atom"))
+        self._template = Types.NotEmpty(Types.NotNone(node.get("template")))
 
-        rootUpdated = ET.SubElement(root, NS.PyBlog.updated)
-        rootUpdated.text = max(map(updatedKey, posts)).isoformat() + "Z"
-        rootPath = ET.SubElement(root, getattr(NS.PyBlog, "feed-path"))
-        rootPath.text = ctx.FullURI
-        rootNode = ET.SubElement(root, getattr(NS.PyBlog, "node-path"))
-        rootNode.text = path
-        rootBlog = ET.SubElement(root, getattr(NS.PyBlog, "blog-path"))
-        rootBlog.text = self.blog.Path
-        for post in posts:
-            postNode = ET.SubElement(root, NS.PyBlog.post)
-            postID = ET.SubElement(postNode, NS.PyBlog.id)
-            postID.text = self.idPrefix + post.Path
-            postUpdated = ET.SubElement(postNode, NS.PyBlog.updated)
-            postUpdated.text = updatedKey(post).isoformat() + "Z"
-            postNode.append(post.getArticle())
-
-        self.Site.transformReferences(ctx, root)
-        feed = self.template.rawTransform(root, {})
-        self.Site.transformPyNamespace(ctx, feed, crumbs=False, link=False)
+    def transform(self, ctx, root):
+        feed = super(Atom, self).transform(ctx, root)
+        transformHref = self.Site.transformHref
         for localLink in feed.iter(NS.PyWebXML.link):
             localLink.tag = NS.Atom.link
-            self.Site.transformHref(ctx, localLink)
-            localLink.text = self.linkPrefix[:-1]+localLink.get("href")
+            transformHref(ctx, localLink)
+            localLink.text = self._linkPrefix[:-1]+localLink.get("href")
             del localLink.attrib["href"]
         return feed
 
-class AtomFeedNode(Nodes.Node):
-    def __init__(self, blogDir, title, kind):
-        super(AtomFeedNode, self).__init__(blogDir.Site, blogDir, None)
-        self.directory = blogDir
-        self.title = title
-        self.kind = kind
-
-    def resolvePath(self, ctx, relPath):
-        self.feedRoot = self.directory.blog.atomFeed
-        if self.feedRoot is None:
-            raise Errors.NotFound()
-        result = super(AtomFeedNode, self).resolvePath(ctx, relPath)
-        if result is self:
-            self.posts = self.directory.getPosts()
-            self.feedRoot.prepare(ctx, self.posts)
-        return result
-
-    def getContentType(self, ctx):
-        return ContentTypes.Atom
-
-    def handle(self, ctx):
-        return self.feedRoot.render(ctx, self.posts, self.title, self.kind,
-                self.directory.Path)
-
-    def getNavigationInfo(self, ctx):
-        return None
-
+    # these are handled by a proxy class. this instance is _never_ returned as
+    # handling node
+    requestHandlers = {}

PyWeblog/Directories.py
@@ -1,174 +1,190 @@
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import, print_function
 
-import os, mimetypes, abc, itertools, copy
 from datetime import datetime
-import time
+import functools, copy
 
 from PyXWF.utils import ET
-import PyXWF.utils as utils
-import PyXWF.Registry as Registry
-import PyXWF.Navigation as Navigation
-import PyXWF.Types as Types
-import PyXWF.Errors as Errors
 import PyXWF.Nodes as Nodes
 import PyXWF.Navigation as Navigation
+import PyXWF.Errors as Errors
 import PyXWF.Namespaces as NS
 
-class BlogFakeDir(Nodes.DirectoryResolutionBehaviour, Nodes.Node, Navigation.Info):
-    def __init__(self, blog, parent, name=None, node=None):
-        if node is None and name is None:
-            raise ValueError("One of name and node must be set")
-        super(BlogFakeDir, self).__init__(blog.Site, parent, node)
-        self.blog = blog
+import PyWeblog.Post as Post
+import PyWeblog.Protocols as Protocols
+
+@functools.total_ordering
+class YearDir(Nodes.DirectoryResolutionBehaviour, Nodes.Node, Navigation.Info):
+    def __init__(self, blog, year):
+        super(YearDir, self).__init__(blog.Site, blog, None)
+        self.Blog = blog
+        self._name = unicode(year)
+        self._path = self.Parent.Path + unicode(year)
+        self.year = year
+        self._yearStr = unicode(year)
+        self._navDisplay = Navigation.Show if blog._nestMonths \
+                            else Navigation.ReplaceWithChildren
+        self._months = [None] * 12
+
+    def _getChildNode(self, key):
+        if key == "":
+            newKey = self._getFirstValidMonth()
+            if newKey is None:
+                return None
+            raise Errors.Found(local=True, location=unicode(newKey+1))
+
+        try:
+            month = int(key)
+        except ValueError:
+            return None
+        if not 1 <= month <= 12:
+            return None
+
+        return self._months[month-1]
+
+    def _getFirstValidMonth(self):
+        for i, month in enumerate(self._months):
+            if month is not None:
+                return i
+        else:
+            return None
+
+    def autocreateMonthNode(self, month):
+        monthIdx = month-1
+        node = self._months[monthIdx]
         if node is None:
-            self._name = unicode(name)
-            self._path = parent.Path + "/" + self._name
-
-    def getTitle(self):
-        return self._name
-
-    def getDisplay(self):
-        return Navigation.Show if len(self) > 0 else Navigation.Hidden
-
-    def getRepresentative(self):
-        return self
+            node = MonthDir(self, monthIdx+1)
+            self._months[monthIdx] = node
+        node.updateChildren()
+        return node
 
     def getNavigationInfo(self, ctx):
         return self
 
-class BlogYearDir(BlogFakeDir):
-    def __init__(self, blog, year):
-        super(BlogYearDir, self).__init__(blog, blog, str(year))
-        self._year = year
-        self.months = [None] * 12
-        self.monthType = Types.NumericRange(int, 1, 12)
-
-    def _validMonthIter(self, reverse=False):
-        if reverse:
-            return itertools.ifilter(lambda x: x is not None, reversed(self.months))
-        else:
-            return itertools.ifilter(lambda x: x is not None, self.months)
-
-    def remove(self, post):
-        month = post.creationDate.month
-        monthDir = self.months[month]
-        monthDir.remove(post)
-
-    def doGet(self, ctx):
-        try:
-            highestMonth = next(iter(self._validMonthIter(reverse=True)))
-        except StopIteration:
-            raise Errors.NotFound()
-        raise Errors.Found(location=highestMonth.Path)
+    def getTitle(self):
+        return self.yearStr
 
     def getDisplay(self):
-        result = super(BlogYearDir, self).getDisplay()
-        if self.blog.combinedStyle and result is Navigation.Show:
-            return Navigation.ReplaceWithChildren
-        else:
-            return result
+        return self._navDisplay
 
-    def _getChildNode(self, key):
-        if key == "":
-            return self
-        else:
-            try:
-                return self.months[int(key)]
-            except (ValueError, TypeError) as err:
-                return None
+    def getRepresentative(self):
+        return self
 
-    def __getitem__(self, month):
-        month = self.monthType(month) - 1
-        monthObj = self.months[month]
-        if monthObj is None:
-            monthObj = BlogMonthDir(self, month)
-            self.months[month] = monthObj
-        return monthObj
+    def purgeEmpty(self):
+        for i, month in enumerate(self._months):
+            if month is None:
+                continue
+            if len(month) == 0:
+                self._months[i] = None
 
     def __iter__(self):
-        return iter(self._validMonthIter(reverse=True))
+        return (month for month in reversed(self._months) if month is not None)
 
     def __len__(self):
-        return sum(1 for _ in self._validMonthIter())
+        return sum(1 for month in self._months if month is not None)
 
-    requestHandlers = {
-        "GET": doGet
-    }
+    def __lt__(self, other):
+        try:
+            return self.year < other.year
+        except AttributeError:
+            return NotImplemented
 
-class BlogMonthDir(BlogFakeDir):
-    __metaclass__ = Nodes.NodeMeta
+    def __le__(self, other):
+        try:
+            return self.year <= other.year
+        except AttributeError:
+            return NotImplemented
 
-    def __init__(self, yearDir, month):
-        super(BlogMonthDir, self).__init__(yearDir.blog, yearDir, str(month))
-        self._month = month
-        self.pathDict = {}
-        self.children = []
-        self._fullMonthName = time.strftime("%B %Y",
-            (self.Parent._year, self._month, 1, 0, 0, 0, 0, 0, -1))
-        self._monthName = time.strftime("%B",
-            (2000, self._month, 1, 0, 0, 0, 0, 0, -1))
+    def __eq__(self, other):
+        try:
+            return self.year == other.year
+        except AttributeError:
+            return NotImplemented
 
-    def add(self, post):
-        if post.Name in self.pathDict:
-            raise ValueError("Duplicate name: {0}".format(post.Name))
-        self.children.append(post)
-        self.children.sort(key=lambda x: x.creationDate, reverse=True)
-        self.pathDict[post.Name] = post
+class MonthDir(Nodes.DirectoryResolutionBehaviour, Nodes.Node, Navigation.Info,
+        Protocols.PostDirectory):
 
-    def remove(self, post):
-        del self.pathDict[post.Name]
-        self.children.remove(post)
+    SelectionCriterion = "month"
 
-    def doGet(self, ctx):
-        ctx.useResources(list(self.children))
-        abstractList = ET.Element(getattr(NS.PyBlog, "abstract-list"), attrib={
-            "kind": "month",
-            "title": self._fullMonthName
-        })
-        for post in self.children:
-            abstractList.append(post.getAbstract(ctx))
-        doc = self.blog.AbstractListTemplate.transform(abstractList, {})
-        return doc
-
-    def getTitle(self):
-        if self.blog.combinedStyle:
-            return self._fullMonthName
+    def __init__(self, yearNode, month):
+        blog = yearNode.Blog
+        super(MonthDir, self).__init__(blog.Site, yearNode, None)
+        self.Blog = blog
+        self.index = blog.index
+        self._childMap = {}
+        self._children = []
+        self._name = unicode(month)
+        self._path = self.Parent.Path + unicode(month)
+        self.month = month
+        self.year = yearNode.year
+        # FIXME: allow user/locale defined month names
+        self._monthStr = datetime(2000, month, 1).strftime("%B")
+        self._monthName = "{0} {1}".format(self._monthStr, yearNode.year)
+        if blog._nestMonths:
+            self._title = self._monthStr
         else:
-            return self._monthName
+            self._title = self._monthName
 
     def _getChildNode(self, key):
         if key == "":
             return self
-        return self.pathDict.get(key, None)
+        else:
+            return self._childMap.get(key, None)
+
+    @property
+    def SelectionValue(self):
+        return self._monthName
+
+    def doGet(self, ctx):
+        return self.Site.templateCache[self.Blog.monthTemplate].transform(
+            self.abstracts,
+            self.Blog.getTransformArgs()
+        )
+
+    def getNavigationInfo(self, ctx):
+        return self
+
+    def getTitle(self):
+        return self._title
+
+    def getDisplay(self):
+        return Navigation.Show
+
+    def getRepresentative(self):
+        return self
+
+    def getPosts(self):
+        return self._children
+
+    def updateChildren(self):
+        abstracts = ET.Element(getattr(NS.PyBlog, "abstract-list"), attrib={
+            "month-name": self._monthStr,
+            "month": unicode(self.month),
+            "year": unicode(self.year)
+        })
+        self._childMap.clear()
+        self._children = []
+        for post in reversed(self.Blog.index.getPostsByMonth(self.year, self.month)):
+            name = post.basename
+            if name in self._childMap:
+                raise ValueError("Conflict: Duplicate post name: {0}".format(name))
+            node = Post.PostNode(self, post)
+            self._childMap[name] = node
+            self._children.append(node)
+            abstracts.append(copy.deepcopy(post.abstract))
+        self.abstracts = abstracts
 
     def __iter__(self):
-        if self.blog.showPostsInNav:
-            return iter(self.children)
+        if self.Blog.showPostsInNav:
+            return iter(self._children)
         else:
             return iter([])
 
     def __len__(self):
-        return len(self.children)
+        return len(self._children)
 
     requestHandlers = {
         "GET": doGet
     }
 
 
-class WithFeedMixin(Nodes.Node):
-    def resolvePath(self, ctx, relPath):
-        if relPath == "":
-            queryInfo = ctx.QueryData
-            if "feed" in queryInfo:
-                feed = queryInfo["feed"]
-                if len(feed) > 1:
-                    raise Errors.BadRequest()
-                feed = feed[0]
-                if feed == "atom":
-                    return self.feedNode.resolvePath(ctx, "")
-                else:
-                    raise Errors.NotFound()
-            elif not queryInfo:
-                return super(WithFeedMixin, self).resolvePath(ctx, relPath)
-            else:
-                raise Errors.BadRequest()
+

commit 11f681850e553c40c6c9fe1c668a1b4cefe643b8

Fix late regression from 16acfe43

PyWeblog/Index.py
@@ -145,6 +145,8 @@ class Post(Resource.Resource):
                 "href": nextpost.path,
                 "title": nextpost.title
             })
+
+        ET.SubElement(meta, getattr(NS.PyBlog, "node-path")).text = self.path
         return page
 
     def __lt__(self, other):