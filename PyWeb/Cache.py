de207b9d388b02bf2829fa3fdda173712b8751bb
Jonas Wielicki <j.wielicki@sotecware.net>
2012-09-10 15:52:54 +0200

    Documentation for Cache.py

--- PyWeb/Cache.py
+++ PyWeb/Cache.py
@@ -1,5 +1,9 @@
 """
+Caching framework for PyWeb. This is probably not as mature as we may need it
+in the future, but it provides basic means for caching resources used by the
+PyWeb instance.
 
+It may, in the future, also be used to cache complete rendered pages.
 """
 
 import abc, functools, time, os
@@ -9,22 +13,46 @@ import PyWeb.Errors as Errors
 import PyWeb.utils as utils
 
 class Cachable(object):
+    """
+    Represents an object which can reside in a cache. This class defines the
+    interface neccessary for a cache entry to work properly. The following
+    attributes are used (and reserved by) the caching framework on Cachables:
+
+    * `_cache_lastAccess` -- timestamp of last access of the object via the
+      cache. This is used as a metric of when an entry can be uncached if limits
+      are reached.
+    * `_cache_master` -- The :cls:`Cache` instance holding the object.
+    """
+
     __metaclass__ = abc.ABCMeta
 
     def __init__(self):
         super(Cachable, self).__init__()
         self._cache_lastAccess = time.time()
+        self._cache_master = None
 
     def touch(self):
+        """
+        Set the internal timestamp of last use to the current timestamp. This
+        will also inform the master (if known) of the changed value for uncache
+        metrics.
+        """
         self._cache_lastAccess = time.time()
         if self._cache_master is not None:
             self._cache_master._changed(self)
 
     def uncache(self):
+        """
+        Remove the object from the cache it is associated to.
+        """
         if self._cache_master is not None:
             self._cache_master._remove(self)
 
     def proposeUncache(self):
+        """
+        Set the timestamp of last use in the past so that uncaching of this
+        object in case of reached limits is more likely than for other objects.
+        """
         # it must be something which is definetly in the past (a date which is
         # easier to find than a date which is definetly in the future. Greetings
         # to MTA folks).
@@ -35,22 +63,50 @@ class Cachable(object):
         return self._cache_lastAccess
 
 class SubCache(object):
+    """
+    The big master cache (:cls:`Cache` instance) is subdivided into smaller
+    parts, :cls:`SubCache` instances, which can be thought of namespaces inside
+    the cache.
+
+    They're used to separate different kinds of cachable objects, which can be
+    handled by different `SubCache`-derived classes. See
+    :cls:`FileSourcedCache` as an example for optimizations possible using
+    this.
+
+    SubCaches provide dictionary-like access to cachables, associating *keys*
+    to the cachables. An object can be added to the cache by simply assigning
+    a key to it. Objects can also be uncached by using the `del` operator. The
+    `in` operator and the `len` function work properly.
+    """
+
     def __init__(self, cache):
         self.master = cache
         self.entries = {}
         self.reverseMap = {}
 
     def _kill(self, cachable):
+        """
+        Delete a cache entry from the subcache. Do not call this method. Uncache
+        entries using :meth:`remove()`, :meth:`Cachable.uncache()` or
+        :meth:`Cache.remove()`.
+        """
         del self.entries[self.reverseMap[cachable]]
         del self.reverseMap[cachable]
 
     def getDefault(self, key, default=None):
+        """
+        Try to get an object from the cache and return *default* (defaults to
+        ``None``) if no object is associated with *key*.
+        """
         try:
             return self[key]
         except KeyError:
             return default
 
     def remove(self, cachable):
+        """
+        Remove a cachable from the Cache.
+        """
         self.master.remove(cachable)
 
     def __getitem__(self, key):
@@ -91,21 +147,40 @@ class SubCache(object):
 
 
 class Cache(object):
+    """
+    Master object representing a full application cache. Objects are not
+    directly stored in the :cls:`Cache` instance but in sub-caches. The
+    :cls:`Cache` object provides dictionary-like access to sub caches. If no
+    cache is associated with a certain key, a new raw :cls:`SubCache` is created
+    for that key.
+
+    Specialized sub caches can be created using :meth:`specializedSubcache`.
+    """
     def __init__(self, limit=0):
         self.subCaches = {}
         self._limit = 0
         self.Limit = limit
 
     def _add(self, cachable):
+        """
+        Add a cachable. Do not call this directly. Only used for bookkeeping.
+        """
         if self._limit:
             self.entries.append(cachable)
 
     def _changed(self, entry):
+        """
+        Resort the container keeping track of all entries to enforce cache
+        limits.
+        """
         if not self._limit:
             return
         self.entries.sort()
 
     def _remove(self, cachable):
+        """
+        Remove a cachable from the cache.
+        """
         if self._limit:
             # no need to resort here
             self.entries.remove(cachable)
@@ -128,6 +203,13 @@ class Cache(object):
         del self.subCaches[key]
 
     def specializedCache(self, key, cls, *args, **kwargs):
+        """
+        Create a specialized subcache using the given class *cls* at the given
+        *key*. Further arguments and keyword arguments are passed to the
+        constructor of *cls*.
+
+        Return the new *cls* instance.
+        """
         cache = cls(self, *args, **kwargs)
         if key in self.subCaches:
             raise KeyError("SubCache key already in use: {0}".format(key))
@@ -187,6 +269,15 @@ class Cache(object):
 
 
 class FileSourcedCache(SubCache):
+    """
+    This is an abstract baseclass for caches which are backed by files on the
+    file system. The file names are used as keys relative to a given root
+    directory *rootPath*.
+
+    A deriving class has to implement the *_load* method which is called if a
+    file accessed through this cache is not available in the cache.
+    """
+
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, master, rootPath):
