zombofant/pyxwf

commit e055ffe88548bdf9edf9a78e70d960daeaaed7fc

Implement use of XSL transform instead of py loops

data/final-transform.xsl
@@ -55,17 +55,23 @@
                 <xsl:value-of select="$href" />
             </xsl:when>
             <xsl:otherwise>
-                <xsl:value-of select="$localr_prefix" />
+                <xsl:value-of select="$url_root" />
                 <xsl:value-of select="$href" />
             </xsl:otherwise>
         </xsl:choose>
+    </xsl:template>
+
+    <xsl:template name="py-host-prefix">
+        <xsl:value-of select="$url_scheme" />
+        <xsl:text>://</xsl:text>
+        <xsl:value-of select="$host_name" />
     </xsl:template>
 
     <xsl:template name="py-transform-localg-href">
         <xsl:param name="href" select="@href" />
         <xsl:choose>
             <xsl:when test="starts-with(string($href), '/')">
-                <xsl:value-of select="$host_prefix" />
+                <xsl:call-template name="py-host-prefix" />
                 <xsl:value-of select="$href" />
             </xsl:when>
             <xsl:when test="starts-with(string($href), '#')">
@@ -75,8 +81,8 @@
                 <xsl:value-of select="$href" />
             </xsl:when>
             <xsl:otherwise>
-                <xsl:value-of select="$host_prefix" />
-                <xsl:value-of select="$localr_prefix" />
+                <xsl:call-template name="py-host-prefix" />
+                <xsl:value-of select="$url_root" />
                 <xsl:value-of select="$href" />
             </xsl:otherwise>
         </xsl:choose>

tests/test_final_transform.py
@@ -32,7 +32,7 @@ class FinalTransform(Mocks.SiteTest):
     def py_transform(self, body, ctx=None):
         ctx = ctx or self.mock_ctx()
         tree_py = copy.deepcopy(body)
-        self.site.transform_py_namespace(ctx, tree_py)
+        tree_py = self.site.transform_py_namespace(ctx, tree_py)
         return tree_py
 
     def raw_transform(self, body, ctx=None):

PyWeblog/GenericFeed.py
@@ -103,5 +103,5 @@ class GenericFeed(Protocols.Feed):
     def transform(self, ctx, root):
         self.site.transform_references(ctx, root)
         feed = self.site.template_cache[self._template].raw_transform(root, {})
-        self.site.transform_py_namespace(ctx, feed, crumbs=False, link=False)
+        feed = self.site.transform_py_namespace(ctx, feed, crumbs=False)
         return feed

PyXWF/Site.py
@@ -66,6 +66,10 @@ class Site(Resource.Resource):
         self.startcwd = os.getcwd()
         self.default_url_root = default_url_root
         self.cache = Cache.Cache(self)
+        self.final_transform = Templates.XSLTTemplate(
+            self,
+            os.path.join(PyXWF.data_path, "final-transform.xsl")
+        )
         # self.savepoint = ImportSavepoints.RollbackImporter()
         try:
             self.load_sitemap(sitemap_file)
@@ -212,27 +216,7 @@ class Site(Resource.Resource):
         for i, node in enumerate(crumb.render(ctx, parent)):
             parent.insert(idx+i, node)
 
-    def transform_py_if_mobile(self, ctx, body):
-        todelete = set()
-        for mobile_switch in body.iter(getattr(NS.PyWebXML, "if-mobile")):
-            if Types.Typecasts.bool(mobile_switch.get("mobile", True)) != ctx.IsMobileClient:
-                todelete.add(mobile_switch)
-                continue
-            try:
-                xhtmlel = mobile_switch.attrib.pop("xhtml-element")
-            except KeyError:
-                xhtmlel = "span"
-            mobile_switch.tag = getattr(NS.XHTML, xhtmlel)
-            try:
-                del mobile_switch.attrib["mobile"]
-            except KeyError:
-                pass
-        for mobile_switch in todelete:
-            mobile_switch.getparent().remove(mobile_switch)
-
-    def transform_py_namespace(self, ctx, body, crumbs=True, a=True, link=True,
-            img=True, mobile_switch=True, content_attr=True,
-            drop_empty_attr=True):
+    def transform_py_namespace(self, ctx, body, crumbs=True):
         """
         Do PyXWF specific transformations on the XHTML tree *body*. This
         includes transforming local a tags, local img tags and placing crumbs.
@@ -245,8 +229,6 @@ class Site(Resource.Resource):
         See :ref:`<py-namespace>` for documentation on what can be done with
         in that XML namespace.
         """
-        if mobile_switch:
-            self.transform_py_if_mobile(ctx, body)
         while crumbs:
             crumbs = False
             for crumb_node in body.iter(NS.PyWebXML.crumb):
@@ -258,49 +240,17 @@ class Site(Resource.Resource):
                     raise ValueError("Invalid crumb id: {0!r}."\
                             .format(crumb_id))
                 self._place_crumb(ctx, crumb_node, crumb)
-        if mobile_switch:
-            # just in case another py:if-mobile tag was placed by a crumb
-            self.transform_py_if_mobile(ctx, body)
-        if a:
-            for locallink in body.iter(NS.PyWebXML.a):
-                locallink.tag = NS.XHTML.a
-                self.transform_href(ctx, locallink)
-        if img:
-            for localimg in body.iter(NS.PyWebXML.img):
-                localimg.tag = NS.XHTML.img
-                self.transform_href(ctx, localimg)
-                localimg.set("src", localimg.get("href"))
-                del localimg.attrib["href"]
-        if link:
-            for locallink in body.iter(NS.PyWebXML.link):
-                locallink.tag = NS.XHTML.link
-                if locallink.get("href"):
-                    self.transform_href(ctx, locallink)
-        if content_attr:
-            content_attrname = NS.PyWebXML.content
-            content_make_uri_attrname = getattr(NS.PyWebXML, "content-make-uri")
-            for el in body.iterfind(".//*[@{0}]".format(content_attrname)):
-                make_uri = el.get(content_make_uri_attrname)
-                if make_uri is not None:
-                    del el.attrib[content_make_uri_attrname]
-                    make_global = Types.Typecasts.bool(make_uri)
-                else:
-                    make_global = False
-                el.set("content", el.get(content_attrname))
-                del el.attrib[content_attrname]
-                self.transform_href(ctx, el, "content", make_global=make_global)
-        if drop_empty_attr:
-            drop_empty_attrname = getattr(NS.PyWebXML, "drop-empty")
-            for el in list(body.iterfind(".//*[@{0}]".format(drop_empty_attrname))):
-                del el.attrib[drop_empty_attrname]
-                if len(el) == 0:
-                    el.getparent().remove(el)
+        return self.final_transform.raw_transform(
+            body,
+            self.get_template_arguments(ctx)
+        ).getroot()
 
 
     def get_template_arguments(self, ctx):
         # XXX: This will possibly explode one day ...
         return {
             b"site_title": utils.unicode2xpathstr(self.title),
+            b"deliver_mobile": "1" if ctx.IsMobileClient else "0",
             b"mobile_client": "1" if ctx.IsMobileClient else "0",
             b"host_name": utils.unicode2xpathstr(ctx.HostName),
             b"url_scheme": utils.unicode2xpathstr(ctx.URLScheme),

PyXWF/Templates.py
@@ -89,7 +89,7 @@ class Template(Resource.Resource):
         for hmeta in newdoc.hmeta:
             head.append(hmeta)
         html.append(body)
-        self.site.transform_py_namespace(ctx, html)
+        html = self.site.transform_py_namespace(ctx, html)
 
         return ET.ElementTree(html)
 

commit 8b0f51ad7e254da94c37d3febb803ef2b2a377f7

Fix handling of <py:script /> elements
This fixes a regression introduced in
e055ffe88548bdf9edf9a78e70d960daeaaed7fc, which made py:script tags
getting lost in the transformation pipeline.

data/final-transform.xsl
@@ -147,6 +147,19 @@
         </xsl:element>
     </xsl:template>
 
+    <!-- legacy: <py:script /> -->
+    <xsl:template match="py:script">
+        <xsl:element name="h:script">
+            <xsl:if test="@href">
+                <xsl:attribute name="src">
+                    <xsl:call-template name="py-transform-localr-href" />
+                </xsl:attribute>
+            </xsl:if>
+            <xsl:apply-templates select="@*[local-name() != 'href']|node()" />
+            <xsl:value-of select="." />
+        </xsl:element>
+    </xsl:template>
+
     <!-- legacy <py:img /> -->
     <xsl:template match="py:img">
         <h:img>

tests/test_final_transform.py
@@ -10,6 +10,7 @@ from PyXWF.utils import ET
 import PyXWF.utils as utils
 import PyXWF.Namespaces as NS
 import PyXWF.Templates as Templates
+import PyXWF.ContentTypes as ContentTypes
 
 import tests.Mocks as Mocks
 
@@ -17,7 +18,7 @@ class FinalTransform(Mocks.SiteTest):
     def setUp(self):
         super(FinalTransform, self).setUp()
         transform_path = os.path.join(PyXWF.data_path, "final-transform.xsl")
-        self.transform = Templates.XSLTTemplate(None, transform_path)
+        self.transform = Templates.XSLTTemplate(self.site, transform_path)
         self.maxDiff = None
 
     def mock_ctx(self):
@@ -35,8 +36,7 @@ class FinalTransform(Mocks.SiteTest):
         tree_py = self.site.transform_py_namespace(ctx, tree_py)
         return tree_py
 
-    def raw_transform(self, body, ctx=None):
-        ctx = ctx or self.mock_ctx()
+    def _get_transform_args(self, ctx):
         d = self.site.get_template_arguments(ctx)
         d[b"localr_prefix"] = utils.unicode2xpathstr(self.site.urlroot)
         d[b"host_prefix"] = utils.unicode2xpathstr(
@@ -46,7 +46,17 @@ class FinalTransform(Mocks.SiteTest):
             )
         )
         d[b"deliver_mobile"] = str(ctx.IsMobileClient).lower()
+        return d
+
+    def raw_transform(self, body, ctx=None):
+        ctx = ctx or self.mock_ctx()
+        d = self._get_transform_args(ctx)
         return self.transform.raw_transform(body, d)
+
+    def full_fransform(self, document, ctx=None):
+        ctx = ctx or self.mock_ctx()
+        d = self._get_transform_args(ctx)
+        return self.transform.final(ctx, document)
 
     def assertTreeEqual(self, tree_a, tree_b):
         ET.cleanup_namespaces(tree_a)
@@ -139,6 +149,21 @@ class FinalTransform(Mocks.SiteTest):
         link1.tag = NS.XHTML.link
         link2.tag = NS.XHTML.link
         link2.set("href", "/foobar/baz")
+
+        self.assertTreeEqual(tree_xsl, root)
+
+    def test_script(self):
+        root, head, _ = self.base_tree()
+        script = ET.SubElement(head, NS.PyWebXML.script)
+        script.set("href", "/foobar/baz")
+        script.tail = "\n"
+
+        ctx = self.mock_ctx()
+        tree_xsl = self.raw_transform(root, ctx=ctx)
+
+        script.tag = NS.XHTML.script
+        script.set("src", "/foobar/baz")
+        del script.attrib["href"]
 
         self.assertTreeEqual(tree_xsl, root)
 
@@ -241,6 +266,30 @@ class FinalTransform(Mocks.SiteTest):
         del a.attrib[NS.PyWebXML.content]
 
         self.assertTreeEqual(tree_xsl, root)
+
+    def test_preserve_existing(self):
+        pwx = NS.PyWebXML
+        xhtml = NS.XHTML
+        root = pwx("page",
+            pwx("meta",
+                xhtml("script", href="foo")
+            ),
+            xhtml("body", "bar")
+        )
+        document = self.site.parser_registry[ContentTypes.PyWebXML].parse_tree(root)
+
+        ctx = self.mock_ctx()
+        tree_xsl = self.transform.final(ctx, document)
+
+        correct_tree = xhtml("html",
+            xhtml("head",
+                xhtml("title", "None"),
+                xhtml("script", href="foo")
+            ),
+            xhtml("body", "bar")
+        )
+
+        self.assertTreeEqual(tree_xsl, correct_tree)
 
     def tearDown(self):
         del self.transform